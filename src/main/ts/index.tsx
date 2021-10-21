4dd6e3e67f6489da16200c06a316302bd7c8453b
naoene <zhugekongmian114@gmail.com>
2021-10-21 14:16:58 +0900

    ヘッダ表示

--- src/main/ts/index.tsx
+++ src/main/ts/index.tsx
@@ -1,8 +1,35 @@
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 import ReactDom from 'react-dom';
 
 function App(): JSX.Element {
-  return <div>test</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Container>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}>
+                傾国・群雄おにぎり計画
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 ReactDom.render(<App />, document.getElementById('app'));
